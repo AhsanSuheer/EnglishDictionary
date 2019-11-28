316936c9d073fcc3b5fccb978c7dbe4eac38d63e
ahsansuheer <ahsan.suheer@gmail.com>
2019-11-27 21:41:15 -0500

    Added QOL change for case sensitivity and error message for invalid input.

--- EnglishDictionary.py
+++ EnglishDictionary.py
@@ -2,6 +2,19 @@ import json
 
 data = json.load(open("data.json"))
 
+def fetch(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif word.upper() in data: #For case where word might be an acronym
+        return data[word.upper()]
+    elif word.title() in data: #For cases where word is a proper noun
+        return data[word.title()]
+    else:
+        return "Sorry, the word was not found in the dictionary."
+
+
+
 word = input("Enter your word:")
 
-print(data[word])
+print(fetch(word))
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # EnglishDictionary
+
+Simple English dictionary that allows retrieval of definitions of words. Allows case insensitivity and also slight typos by offering similar words.
