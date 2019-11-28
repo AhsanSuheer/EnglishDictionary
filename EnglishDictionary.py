01265e035dde578ec7d65910b315ed4ede334a20
ahsansuheer <ahsan.suheer@gmail.com>
2019-11-27 21:57:59 -0500

    Added feature to fetch closest match to invalid inputs. Fixed printing of words with multiple definitions.

--- EnglishDictionary.py
+++ EnglishDictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -10,11 +11,25 @@ def fetch(word):
         return data[word.upper()]
     elif word.title() in data: #For cases where word is a proper noun
         return data[word.title()]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        userAns = input(f"Did you mean {get_close_matches(word, data.keys())[0]} instead? Reply 'Y' or 'N'")
+        if userAns == 'Y':
+            return data[get_close_matches(word, data.keys())[0]] #Returning the closest match
+        elif userAns == 'N':        
+            return "Sorry, the word was not found in the dictionary."
+        else:
+            return "Invalid answer."
     else:
         return "Sorry, the word was not found in the dictionary."
 
 
 
 word = input("Enter your word:")
+answer = fetch(word)
 
-print(fetch(word))
+#Dealing with words that have multiple definitions
+if type(answer) == list: 
+    for definition in answer:
+        print(definition)
+else:
+    print(answer)
